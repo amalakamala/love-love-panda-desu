b02e6f996f54404f02a43ac24457717ddfb6988d
amalakamala <amalakamala.a@gmail.com>
2017-05-18 14:59:36 -0400

    pandas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+
+var lugar = document.getElementsByClassName('imagenes-varias')[0];
+
+
+pictures.forEach(function(f){
+	var img = document.createElement('img') // <img/>
+	img.setAttribute("src",f);
+	img.setAttribute("class","los-pandas");
+	lugar.appendChild(img);	
+})
+
+
